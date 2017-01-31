22183ad1ae5d5484e61ad6c42aad8a7ec32095ca
wuqingvika <526288022@qq.com>
2017-01-31 21:38:45 +0800

    testwq

--- src/com/wq/test/TestWq.java
+++ src/com/wq/test/TestWq.java
@@ -4,7 +4,7 @@ public class TestWq {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("wuqingvika love coding");
+		System.out.println("wuqingvika love codin g");
 	}
 
 }
